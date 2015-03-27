cb468010688935fad396233778a5781a684bc883
Michał Sadowski <sadowskimichal95@gmail.com>
2015-03-27 15:02:51 +0100

    Postgres migrate. Configuration changes.

--- graduates/settings.py
+++ graduates/settings.py
@@ -115,8 +115,12 @@ class Default(Configuration):
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'graduates_db',
+            'USER': 'root',
+            'PASSWORD': '5jfoi3b6',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
         }
     }
 
--- graduates/test_settings.py
+++ graduates/test_settings.py
@@ -90,11 +90,14 @@ STATIC_URL = '/static/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'graduates_db',
+        'USER': 'root',
+        'PASSWORD': '5jfoi3b6',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
