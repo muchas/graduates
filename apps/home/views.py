bed22a673ce519d7ccdc41cbedb0783c8b79c574
Sławomir Mucha <smucha@Slawomirs-MacBook-Air.local>
2015-04-15 18:14:37 +0200

    UI fixes - redirects, urls, template menu

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import redirect
 from django.views.generic.base import TemplateView
+from apps.accounts.models import User
 
 
 class HomePageView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated():
+            context['members_count'] = User.objects.all().count()
+        return context
+
     def get_template_names(self):
         if self.request.user.is_authenticated():
             return ["home/application.html"]
