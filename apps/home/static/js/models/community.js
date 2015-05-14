b334ec8a97c3c1fe4e234740f9df72be35f5ec0b
Sławomir Mucha <smucha@Slawomirs-MacBook-Air.local>
2015-05-14 23:16:24 +0200

    API modifications - retrieving universities and companies connected with city. Switching beetwen three subviews - js side.

--- apps/home/static/js/models/community.js
+++ apps/home/static/js/models/community.js
@@ -3,6 +3,10 @@ App.Model.Group = Backbone.Model.extend({});
 App.Model.Invitation = Backbone.Model.extend({});
 App.Model.City = Backbone.DeepModel.extend({});
 App.Model.Support = Backbone.Model.extend({});
+App.Model.Company = Backbone.Model.extend({});
+App.Collection.Companies = Backbone.Collection.extend({
+   model: App.Model.Company
+});
 App.Collection.Teachers = Backbone.Collection.extend({
     model: App.Model.Teacher
 });
--- apps/home/static/js/views/item/community.js
+++ apps/home/static/js/views/item/community.js
@@ -14,37 +14,95 @@ App.ItemView.CityPerson = Marionette.ItemView.extend({
    template: Handlebars.templates.city_person
 });
 
+App.ItemView.CityUniversity = Marionette.ItemView.extend({
+   template: Handlebars.templates.city_university
+});
+
+App.ItemView.CityCompany = Marionette.ItemView.extend({
+   template: Handlebars.templates.city_company
+});
+
+App.ItemView.EmptyCityCompany = Marionette.ItemView.extend({
+   template: Handlebars.templates.city_company_empty
+});
+
+App.ItemView.EmptyCityUniversity = Marionette.ItemView.extend({
+   template: Handlebars.templates.city_university_empty
+});
+
+App.ItemView.EmptyCityPerson = Marionette.ItemView.extend({
+   template: Handlebars.templates.city_person_empty
+});
+
+
 App.ItemView.City = Marionette.ItemView.extend({
    template: Handlebars.templates.city,
 
     ui: {
        'select': '.year',
-       'people': '#people'
+       'selectBox': '.filter-box',
+       'container': '.city-container',
+       'people': '.people',
+       'companies': '.companies',
+       'universities': '.universities'
     },
 
     events: {
-        'change @ui.select': 'filter'
+        'change @ui.select': 'filter',
+        'click @ui.people': 'renderPeople',
+        'click @ui.companies': 'renderCompanies',
+        'click @ui.universities': 'renderUniversities'
     },
 
     initialize: function() {
         this.peopleCollection = new App.Collection.People(this.model.get('people'));
         this.peopleView = new App.CollectionView.CityPeople({ collection: this.peopleCollection });
+
+        this.companiesCollection = new App.Collection.Companies(this.model.get('companies'));
+        this.companiesView = new App.CollectionView.CityCompanies({ collection: this.companiesCollection });
+
+        this.universitiesCollection = new App.Collection.Universities(this.model.get('universities'));
+        this.universitiesView = new App.CollectionView.CityUniversities({ collection: this.universitiesCollection });
+
+        this.view = null;
+        this.collection = null;
     },
 
     onRender: function() {
-        this.ui.people.html(this.peopleView.render().$el);
+        this.renderPeople();
+    },
+
+    renderContainer: function(collection, view) {
+        this.collection = collection;
+        this.view = view;
+        this.ui.container.hide();
+        this.ui.container.html(this.view.render().$el);
+        this.ui.container.fadeIn(300);
+    },
+
+    renderPeople: function() {
+        this.ui.selectBox.show();
+        this.renderContainer(this.peopleCollection, this.peopleView);
+    },
+
+    renderCompanies: function() {
+        this.ui.selectBox.hide();
+        this.renderContainer(this.companiesCollection, this.companiesView);
+    },
+
+    renderUniversities: function() {
+        this.ui.selectBox.hide();
+        this.renderContainer(this.universitiesCollection, this.universitiesView);
     },
 
     filter: function(e) {
         var year = Number(this.ui.select.val());
         var people = this.model.get('people');
         if(year) {
-            this.peopleCollection.reset(_.where(people, { year: year }));
+            this.collection.reset(_.where(people, { year: year }));
         } else {
-            this.peopleCollection.reset(people);
+            this.collection.reset(people);
         }
-        this.ui.people.hide();
-        this.ui.people.html(this.peopleView.render().$el);
-        this.ui.people.fadeIn(400);
+        this.renderContainer(this.peopleCollection, this.peopleView);
     }
 });
