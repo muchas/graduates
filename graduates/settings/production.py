a6420d199debf4c98b03df93ad647caa5ec10ce5
Sławomir Mucha <smucha@Slawomirs-MacBook-Air.local>
2015-08-26 21:56:11 +0200

    Fix static root in production settings

--- graduates/settings/production.py
+++ graduates/settings/production.py
@@ -8,3 +8,5 @@ EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 MANDRILL_API_KEY = get_env_variable("MANDRILL_API_KEY")
 
 INTERCOM_APP_ID = get_env_variable("INTERCOM_APP_ID")
+
+STATIC_ROOT = os.path.join(BASE_DIR, '/../../', 'static')
