9610c1d64b6e40144ed65def4a9c926155c95e5e
Sławomir Mucha <smucha@Slawomirs-MacBook-Air.local>
2015-09-01 16:08:46 +0200

    Add jubilee registration form

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Feedback, Guest
+from .models import Feedback, Guest, JubileeBooking
 
 admin.site.register(Feedback)
 admin.site.register(Guest)
+
+
+@admin.register(JubileeBooking)
+class JubileeBookingAdmin(admin.ModelAdmin):
+    list_display = ('person', 'gifts_included', 'email')
